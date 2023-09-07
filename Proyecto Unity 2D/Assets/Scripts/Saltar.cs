0437fb78fda0e8c9521e725593295fdb45dbbc02
Lauti <Lautaroeugster@hotmail.com.ar>
2023-09-07 03:39:27 -0300

    Música y sonidos del Jugador
    Canción de fondo y efectos de sonido para el salto y colisión del jugador.

--- Proyecto Unity 2D/Assets/Scripts/Saltar.cs
+++ Proyecto Unity 2D/Assets/Scripts/Saltar.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class Saltar : MonoBehaviour
 {
 
-    // Variables a configurar desde el editor
+    //Variables a configurar desde el editor
     [Header("Configuracion")]
     [SerializeField] private float fuerzaSalto = 5f;
 
-    // Variables de uso interno en el script
+    [SerializeField] private AudioClip jumpSFX;
+    [SerializeField] private AudioClip collisionSFX;
+
+    //Variables de uso interno en el script
     private bool puedoSaltar = true;
     private bool saltando = false;
 
-    // Variable para referenciar otro componente del objeto
+    //Variable para referenciar otro componente del objeto
     private Rigidbody2D miRigidbody2D;
+    private AudioSource miAudioSource;
 
-    // Codigo ejecutado cuando el objeto se activa en el nivel
+    //Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAudioSource = GetComponent<AudioSource>();
     }
 
-    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    //Codigo ejecutado en cada frame del juego (Intervalo variable)
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && puedoSaltar)
         {
             puedoSaltar = false;
+            if(miAudioSource.isPlaying) { return; }
+            miAudioSource.PlayOneShot(jumpSFX);
         }
     }
 
@@ -40,11 +47,14 @@ public class Saltar : MonoBehaviour
         }
     }
 
-    // Codigo ejecutado cuando el jugador colisiona con otro objeto
+    //Codigo ejecutado cuando el jugador colisiona con otro objeto
     private void OnCollisionEnter2D(Collision2D collision)
     {
         puedoSaltar = true;
         saltando = false;
+
+        if (miAudioSource.isPlaying) { return; }
+        miAudioSource.PlayOneShot(collisionSFX);
     }
 
 }
