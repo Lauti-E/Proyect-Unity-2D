4a40f5220d7af87dfe5bf181281c6448e87a9257
Lauti <Lautaroeugster@hotmail.com.ar>
2023-09-27 22:59:55 -0300

    Configuración trampas y meta.
    Al tocar los pinchos se vuelve a la posición inicial y al detectar la meta muestra el mensaje de victoria.

--- Proyecto Unity 2D/Assets/Scripts/Jugador.cs
+++ Proyecto Unity 2D/Assets/Scripts/Jugador.cs
@@ -6,6 +6,14 @@ public class Jugador : MonoBehaviour
 {
     [Header("Configuracion")]
     [SerializeField] private float vida = 5f;
+    private Vector3 posicionInicial; //Almacenará la posición inicial del jugador.
+    private bool enTrampa = false; //Variable de estado para verificar si el jugador colisionó con un pincho.
+
+    private void Start()
+    {
+        //Guardar la posición inicial del jugador al inicio del juego.
+        posicionInicial = transform.position;
+    }
 
     public void ModificarVida(float puntos)
     {
@@ -21,9 +29,15 @@ public class Jugador : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag("Meta")) { return; }
-
-        Debug.Log("GANASTE!");
+        if (collision.gameObject.CompareTag("Trampa")) // Cambia "Trampa" al tag correcto de tus pinchos
+        {
+            // Restablecer la posición del jugador a la posición inicial
+            transform.position = posicionInicial;
+            Debug.Log("Has sido atrapado por la trampa. Vuelve a la posición inicial.");
+        }
+        else if (collision.gameObject.CompareTag("Meta"))
+        {
+            Debug.Log("GANASTE!");
+        }
     }
-
 }
