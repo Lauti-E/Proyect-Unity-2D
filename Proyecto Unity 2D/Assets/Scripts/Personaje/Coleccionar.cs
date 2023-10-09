f13b9ce1bd95382955dd0a3cc95c7e489e3a8696
Lauti <Lautaroeugster@hotmail.com.ar>
2023-10-09 14:58:41 -0300

    Coleccionables en mapa
    Se agregan coleccionables (frutas) las cuales son requisitos para ganar.

--- Proyecto Unity 2D/Assets/Scripts/Personaje/Coleccionar.cs
+++ Proyecto Unity 2D/Assets/Scripts/Personaje/Coleccionar.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coleccionar : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> colleccionables;
+    [SerializeField] private List<GameObject> coleccionables;
     [SerializeField] private GameObject bolsa;
+    public int limiteCol = 5; //Límite de coleccionables.
 
     private void Awake()
     {
-        colleccionables = new List<GameObject>();
+        coleccionables = new List<GameObject>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Coleccionable")) { return; }
 
-        GameObject nuevoColeccionable = collision.gameObject;
-        nuevoColeccionable.SetActive(false);
+        //Verificar si se alcanzó el límite de coleccionables en el inventario.
+        if(coleccionables.Count < limiteCol)
+        {
+            GameObject nuevoColeccionable = collision.gameObject;
+            nuevoColeccionable.SetActive(false);
 
-        colleccionables.Add(nuevoColeccionable);
-        nuevoColeccionable.transform.SetParent(bolsa.transform);
+            coleccionables.Add(nuevoColeccionable);
+            nuevoColeccionable.transform.SetParent(bolsa.transform);
+
+            Debug.Log("Fruta conseguida, ahora tienes: " + coleccionables.Count + " en tu inventario.");
+        }
+        else
+        {
+            Debug.Log("Ya alcanzaste el límite de frutas en tu inventario (" + limiteCol + ").");
+        }
     }
-}
+} 
