35abfb4556d7241d1be4d90f809a0a8e0266ab4e
Lauti <Lautaroeugster@hotmail.com.ar>
2023-10-18 16:13:11 -0300

    Cambios en generadores
    Se modifican los generadores de enemigos completamente.

--- Proyecto Unity 2D/Assets/Scripts/Spawners/GeneradorObjeto.cs
+++ Proyecto Unity 2D/Assets/Scripts/Spawners/GeneradorObjeto.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class GeneradorObjeto : MonoBehaviour
 {
-    public GameObject enemigoPrefab;
+    public GameObject[] enemigoPrefabs; //Arreglo que contiene los prefabs.
 
     private bool generado = false;
+
+    private int enemigosGen = 0; //Contador de enemigos generados.
+    public float tiempoEspera = 5f; //Tiempo de espera en segundos.
 
     private void OnBecameVisible()
     {
         if (!generado && Camera.main.CompareTag("MainCamera"))
         {
             //Iniciar el bucle de generación de enemigos cada 5 segundos.
-            InvokeRepeating("GenerarEnemigo", 0f, 5f);
-
-            GenerarEnemigo();
+            InvokeRepeating("GenerarEnemigo", 0f, tiempoEspera);
+          
+            generado = true;
         }
     }
 
     private void GenerarEnemigo()
     {
-        //Genera el enemigo en la posición del generador.
-        GameObject nuevoEnemigo = Instantiate(enemigoPrefab, transform.position, Quaternion.identity);
+        if (enemigosGen < 3 && enemigoPrefabs.Length > 0)
+        {
+            //Seleccionar un índice aleatorio para el arreglo de prefabs.
+            int indiceAleatorio = Random.Range(0, enemigoPrefabs.Length);
 
-        generado = true;
+            //Genera el enemigo en la posición del generador.
+            GameObject nuevoEnemigo = Instantiate(enemigoPrefabs[indiceAleatorio], transform.position, Quaternion.identity);
+
+            //Incrementar el contador de enemigos generados.
+            enemigosGen++;
+        }
     }
 }
