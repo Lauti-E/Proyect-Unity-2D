4e4ca34bcd0a947986f9e50d0d70dbce7b5e3ed0
Lauti <Lautaroeugster@hotmail.com.ar>
2023-11-01 18:28:17 -0300

    Configuraciones
    Configuraciones en generadores de proyectiles.

--- Proyecto Unity 2D/Assets/Scripts/Enemigos/Herir.cs
+++ Proyecto Unity 2D/Assets/Scripts/Enemigos/Herir.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Herir : MonoBehaviour
 {
-    // Variables a configurar desde el editor
+    //Variables a configurar desde el editor
     [Header("Configuracion")]
     [SerializeField] int puntos = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             Jugador jugador = collision.gameObject.GetComponent<Jugador>();
+
             jugador.ModificarVida(-puntos);
+
             Debug.Log(" PUNTOS DE DAÑO REALIZADOS AL JUGADOR " + puntos);
+
+            if (gameObject.CompareTag("Proyectil"))
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
--- Proyecto Unity 2D/Assets/Scripts/Spawners/ConfigProyectil.cs
+++ Proyecto Unity 2D/Assets/Scripts/Spawners/ConfigProyectil.cs
@@ -12,7 +12,19 @@ public class ConfigProyectil : MonoBehaviour
         //Obtener el Rigidbody2D del proyectil.
         Rigidbody2D rbProyectil = GetComponent<Rigidbody2D>();
 
-        //Asignar velocidad inical al proyectil.
-        rbProyectil.velocity = transform.right * velProyectil;
+        //Asignar velocidad inicial al proyectil.
+        rbProyectil.velocity = new Vector2(0, -velProyectil);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            //Destruir el proyectil al colisionar con el suelo.
+            if (gameObject.CompareTag("Proyectil"))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
